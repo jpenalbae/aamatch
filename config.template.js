d31bac609b0e9acd9c9abbc165af388b1ed6e7b0
Jaime <jpenalbae@gmail.com>
2023-05-12 18:19:11 +0200

    Add https support

--- config.template.js
+++ config.template.js
@@ -5,12 +5,14 @@ const options = {
     sessionSecret: 'your_magic secret',
     hostname: 'localhost',
     listenPort: 3000,
-    url: 'http://localhost:3000',
+    hostname: 'localhost',
     usersDB:  path.join(__dirname, 'data/users.db'),
     sessionExpiration: 1000 * 60 * 60 * 24 * 30, // One month (in milliseconds)
     httpCache: 1000 * 60 * 60 * 24 * 7, // One week (in milliseconds)
     queueTimeout: 1000 * 60 * 30, // 30 minutes (in milliseconds)
     secure: false, // Change this on production to enable https
+    secureCert: path.join(__dirname, 'data/cert.pem'),
+    secureKey: path.join(__dirname, 'data/key.pem'),
     banTime: 3600000, // One hour (in milliseconds) Time to ban after rate limit
 };
 
--- index.js
+++ index.js
@@ -1,8 +1,85 @@
-const app = require('./app');
+const express = require('express');
+const cookieSession = require('cookie-session');
+const expressWs = require('express-ws')
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
+
+const { wsMatchHandler,  wsQueueHandler } = require('./routes/wshandler');
+const publicRouter = require('./routes/api_public');
+const authRouter = require('./routes/api_authenticated');
+const pagesRouter = require('./routes/pages');
 const options = require('./config');
 
 const port = options.listenPort;
 
+const sessionOpts = {
+    secret: options.sessionSecret,
+    cookie: {
+        secure: options.secure, // Change this on production
+        samesite: true,
+        httpOnly: true,
+        maxAge: options.sessionExpiration,
+        path: '/'
+    }
+};
 
-// Start server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Make the URL
+let proto = "http://"
+if (options.secure)
+    proto = "https://"
+options.url = proto + options.hostname + ":" + port;
+
+let maxAge = 0;
+if (process.env.NODE_ENV === 'production')
+    maxAge = options.httpCache;
+
+let server = undefined;
+const app = express();
+if (options.secure) {
+    const httpsOptions = {
+        key: fs.readFileSync(options.secureKey),
+        cert: fs.readFileSync(options.secureCert)
+    };
+    server = https.createServer(httpsOptions, app);
+}
+const wsInstance = expressWs(app, server, {leaveRouterUntouched: true});
+wsInstance.applyTo(authRouter);
+
+// Livereload
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Enable livereload');
+
+    app.use(require('connect-livereload')());
+    
+    const liveReloadServer = require('livereload').createServer();
+    liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 600);
+    });
+}
+
+// Basic express setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.disable('x-powered-by');
+app.use(cookieSession(sessionOpts));
+app.use(express.json());
+app.use(express.static('public', {maxAge: maxAge}));
+app.use('/', pagesRouter);
+app.use('/api/p/', publicRouter);
+app.use('/api/a/', authRouter);
+authRouter.ws('/queue', wsQueueHandler);
+authRouter.ws('/match/:matchid', wsMatchHandler);
+
+
+if (options.secure) {
+    server.listen(port, () => {
+        console.log(`listening on: ${options.url}`);
+    });
+} else {
+    app.listen(port, () => {
+        console.log(`listening on: ${options.url}`);
+    });
+}
